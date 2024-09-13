6933c640d0678893bc2f567cc58f08216c459919
Eugene <eugvok@gmail.com>
2024-09-13 12:10:16 +0300

    finish revers integer task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse(n) {
+   let numAr = Array.from(String(Math.abs(n)), Number);
+   let revAr = []
+   for (let i = 0; i < numAr.length; i++) {
+      revAr[numAr.length - i] = numAr[i];
+   }
+   return +revAr.join('');
 }
+
